9de68a5389e9d79e178a559ad4156a86f2a2c13e
srikk <122169588+py-code314@users.noreply.github.com>
2025-10-30 14:56:11 -0400

    feat: Use NavLink for navbar items
    - Clicking item takes the user to corresponding page.
    - Apply active link styling to clicked items.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
-import { Outlet } from 'react-router'
+import { Outlet, NavLink } from 'react-router'
 import './App.css'
 
 // TODO: Style header and nav bar
- 
+
 function App() {
   return (
-    <div className='page'>
-     
-      <header className='header'>
+    <div className="page">
+      <header className="header">
         {/* <img className='logo' src="" alt="" /> */}
-        <h1 className='title'>Shopping Cart Project</h1>
-        <nav className='navbar'>
-          <ul className='navbar__list'>
-            <li className='navbar__item'>Home</li>
-            <li className='navbar__item'>Shop</li>
-            <li className='navbar__item'>Cart</li>
+        <h1 className="title">Shopping Cart Project</h1>
+        <nav className="navbar">
+          <ul className="navbar__list">
+            <li className="navbar__item">
+              <NavLink
+                to={`home`}
+                className={({ isActive }) => (isActive ? 'active' : '')}>
+                Home
+              </NavLink>
+            </li>
+            <li className="navbar__item">
+              <NavLink
+                to={`shop`}
+                className={({ isActive }) => (isActive ? 'active' : '')}>
+                Shop
+              </NavLink>
+            </li>
+            <li className="navbar__item">
+              <NavLink
+                to={`cart`}
+                className={({ isActive }) => (isActive ? 'active' : '')}>
+                Cart
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </header>
       <Outlet />
-      <footer className='footer'>I am a footer</footer>
+      <footer className="footer">I am a footer</footer>
     </div>
   )
 }
