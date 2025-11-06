c19c995ea051331589a4c98ab83174d950be6705
srikk <122169588+py-code314@users.noreply.github.com>
2025-11-06 12:00:30 -0500

    style: Add styles to ErrorPage component
    - Add background image to the page.

--- src/components/error/ErrorPage.jsx
+++ src/components/error/ErrorPage.jsx
@@ -1,18 +1,27 @@
-import { useRouteError } from "react-router";
+import { useRouteError } from 'react-router'
+import styles from './ErrorPage.module.css'
+import { Link } from 'react-router'
 
 const ErrorPage = () => {
   const error = useRouteError()
   console.error(error)
 
   return (
-    <div className="error">
-      <h1>OOps!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className={styles.error}>
+      <div className={styles.contentWrapper}>
+        <h1>OOps!</h1>
+        <p>Something's wrong here...</p>
+        <p>We can't find the page you're looking for.</p>
+        <p>
+          Go back to{' '}
+          <Link className={styles.linkHome}
+          to={'/home'}>Home</Link>{' '}
+          <br />
+          or go to a <span className={styles.beach}>beach.</span>
+        </p>
+      </div>
     </div>
   )
 }
- 
-export default ErrorPage;
+
+export default ErrorPage
