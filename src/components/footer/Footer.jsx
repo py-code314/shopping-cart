c35cdbe255bc76a6515a5eed9473afe7e2ebf46d
srikk <122169588+py-code314@users.noreply.github.com>
2025-11-07 08:43:50 -0500

    style: Add responsive styles to Footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,132 +10,125 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.contentWrapper}>
         <div className={styles.footerPrimary}>
-          <div className={styles.about}>
+          <div className={`${styles.about} ${styles.card}`}>
             <h2 className={styles.subtitle}>Anchor</h2>
             <p className={styles.description}>
               The full-scale department store experience, now just a click away.
               Shop top brands in fashion, home, and beauty.
             </p>
           </div>
-          <div className={styles.footerGroup}>
-            <div className={styles.groupWrapper}>
-              <h3 className={styles.subheading}>Customer Service</h3>
-              <ul className={styles.footerListPrimary}>
-                <li>
-                  <Link className={`${styles.footerLink} ${styles.inactive}`}>
-                    FAQ and Help
-                  </Link>
-                </li>
-                <li>
-                  <Link className={`${styles.footerLink} ${styles.inactive}`}>
-                    Order Lookup
-                  </Link>
-                </li>
-                <li>
-                  <Link className={`${styles.footerLink} ${styles.inactive}`}>
-                    Returns
-                  </Link>
-                </li>
-                <li>
-                  <Link className={`${styles.footerLink} ${styles.inactive}`}>
-                    Shipping & Delivery
-                  </Link>
-                </li>
-              </ul>
-            </div>
+
+          <div className={styles.card}>
+            <h3 className={styles.subheading}>Customer Service</h3>
+            <ul className={styles.footerListPrimary}>
+              <li>
+                <Link className={`${styles.footerLink} ${styles.inactive}`}>
+                  FAQ and Help
+                </Link>
+              </li>
+              <li>
+                <Link className={`${styles.footerLink} ${styles.inactive}`}>
+                  Order Lookup
+                </Link>
+              </li>
+              <li>
+                <Link className={`${styles.footerLink} ${styles.inactive}`}>
+                  Returns
+                </Link>
+              </li>
+              <li>
+                <Link className={`${styles.footerLink} ${styles.inactive}`}>
+                  Shipping & Delivery
+                </Link>
+              </li>
+            </ul>
           </div>
-          <div className={styles.footerGroup}>
-            <div className={styles.groupWrapper}>
-              <h3 className={styles.subheading}>Stores & Services</h3>
-              <ul className={styles.footerListPrimary}>
-                <li className={styles.footerListItem}>
-                  <Link className={`${styles.footerLink} ${styles.inactive}`}>
-                    Curbside & In Store Pickup
-                  </Link>
-                </li>
-                <li className={styles.footerListItem}>
-                  <Link className={`${styles.footerLink} ${styles.inactive}`}>
-                    Locations & Hours
-                  </Link>
-                </li>
-                <li className={styles.footerListItem}>
-                  <Link className={`${styles.footerLink} ${styles.inactive}`}>
-                    Store Events
-                  </Link>
-                </li>
-                <li className={styles.footerListItem}>
-                  <Link className={`${styles.footerLink} ${styles.inactive}`}>
-                    Gift Registry
-                  </Link>
-                </li>
-              </ul>
-            </div>
+
+          <div className={styles.card}>
+            <h3 className={styles.subheading}>Stores & Services</h3>
+            <ul className={styles.footerListPrimary}>
+              <li className={styles.footerListItem}>
+                <Link className={`${styles.footerLink} ${styles.inactive}`}>
+                  Curbside & In Store Pickup
+                </Link>
+              </li>
+              <li className={styles.footerListItem}>
+                <Link className={`${styles.footerLink} ${styles.inactive}`}>
+                  Locations & Hours
+                </Link>
+              </li>
+              <li className={styles.footerListItem}>
+                <Link className={`${styles.footerLink} ${styles.inactive}`}>
+                  Store Events
+                </Link>
+              </li>
+              <li className={styles.footerListItem}>
+                <Link className={`${styles.footerLink} ${styles.inactive}`}>
+                  Gift Registry
+                </Link>
+              </li>
+            </ul>
           </div>
-          <div className={styles.footerGroup}>
-            <div className={styles.groupWrapper}>
-              <h3 className={styles.subheading}>Follow Us</h3>
-              <ul className={styles.footerListPrimary}>
-                <li className={styles.footerListItem}>
-                  <a
-                    className={styles.footerLink}
-                    href="https://www.facebook.com"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <img src={facebookIcon} alt="" />
-                    <p>Facebook</p>
-                  </a>
-                </li>
-                <li className={styles.footerListItem}>
-                  <a
-                    className={styles.footerLink}
-                    href="https://mastodon.social/explore"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <img src={mastodonIcon} alt="" />
-                    <p>Mastodon</p>
-                  </a>
-                </li>
-                <li className={styles.footerListItem}>
-                  <a
-                    className={styles.footerLink}
-                    href="https://instagram.com"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <img src={instagramIcon} alt="" />
-                    <p>Instagram</p>
-                  </a>
-                </li>
-                <li className={styles.footerListItem}>
-                  <a
-                    className={styles.footerLink}
-                    href="https://pinterest.com"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <img src={pinterestIcon} alt="" />
-                    <p>Pinterest</p>
-                  </a>
-                </li>
-              </ul>
-            </div>
+
+          <div className={styles.card}>
+            <h3 className={styles.subheading}>Follow Us</h3>
+            <ul className={styles.footerListPrimary}>
+              <li className={styles.footerListItem}>
+                <a
+                  className={styles.footerLink}
+                  href="https://www.facebook.com"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <img src={facebookIcon} alt="" />
+                  <p>Facebook</p>
+                </a>
+              </li>
+              <li className={styles.footerListItem}>
+                <a
+                  className={styles.footerLink}
+                  href="https://mastodon.social/explore"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <img src={mastodonIcon} alt="" />
+                  <p>Mastodon</p>
+                </a>
+              </li>
+              <li className={styles.footerListItem}>
+                <a
+                  className={styles.footerLink}
+                  href="https://instagram.com"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <img src={instagramIcon} alt="" />
+                  <p>Instagram</p>
+                </a>
+              </li>
+              <li className={styles.footerListItem}>
+                <a
+                  className={styles.footerLink}
+                  href="https://pinterest.com"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <img src={pinterestIcon} alt="" />
+                  <p>Pinterest</p>
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
         <div className={styles.footerSecondary}>
+          <p className={styles.copyright}>Copyright &copy; 2025</p>
           <ul className={styles.footerListSecondary}>
             <li className={styles.footerListItem}>
-              <p className={styles.copyright}>Copyright &copy; 2025</p>
+              <Link className={`${styles.footerLink} ${styles.inactive}`}>
+                Terms & Conditions
+              </Link>
             </li>
-            <div className={styles.footerSecondaryLinks}>
-              <li className={styles.footerListItem}>
-                <Link className={`${styles.footerLink} ${styles.inactive}`}>
-                  Terms & Conditions
-                </Link>
-              </li>
-              <li className={styles.footerListItem}>
-                <Link className={`${styles.footerLink} ${styles.inactive}`}>
-                  Privacy Policy
-                </Link>
-              </li>
-            </div>
+            <li className={styles.footerListItem}>
+              <Link className={`${styles.footerLink} ${styles.inactive}`}>
+                Privacy Policy
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
